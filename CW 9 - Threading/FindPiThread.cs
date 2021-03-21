71b0a500aef1aa24de0cee6c37d48ae88c81779e
Jordan Taylor <jorrtayl@ut.utm.edu>
2021-03-21 07:41:59 -0500

    Added variables to FindPiThread

---
+++ CW 9 - Threading/FindPiThread.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Threading
+{
+    // Houses thread state and function
+    class FindPiThread
+    {
+        int _dartNum = 0;
+        int _dartsLanded = 100; // temporary
+        Random _genNum = new Random();
+
+        public FindPiThread(int dartNum)
+        {
+            _dartNum = dartNum;
+            int randomNum = _genNum.Next(dartNum, _dartsLanded);
+        }
+    }
+}
--- CW 9 - Threading/Program.cs
+++ CW 9 - Threading/Program.cs
@@ -4,12 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CW_9___Threading
+namespace Threading
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int test1 = 0;
+
+            FindPiThread e = new FindPiThread(test1);
+
+            Console.WriteLine("Enter a number");
+            test1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(e);
+
+            Console.ReadKey();
         }
     }
 }
