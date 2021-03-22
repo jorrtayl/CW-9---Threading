4a59bfe67475435583ac94ee77a76ca8b5d0e219
Jordan Taylor <jorrtayl@ut.utm.edu>
2021-03-22 10:29:09 -0500

    Finished assignment.

--- CW 9 - Threading/FindPiThread.cs
+++ CW 9 - Threading/FindPiThread.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿/* Author: Jordan Taylor
+ * Date: 03 / 22 / 2021
+ * Description: Throws darts and checks if it hits or not.
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,22 +13,44 @@ namespace Threading
     // Houses thread state and function
     class FindPiThread
     {
-        int _dartNum = 0;
-        int _dartsLanded = 100; // temporary
-        Random _genNum = new Random();
+        int _dartNum = 0; // number of darts to throw
+        public int dartsLanded = 0; // darts that land inside
+        private Random _genNum;
 
         public FindPiThread(int dartNum)
         {
-            _dartNum = dartNum;
-            int randomNum = _genNum.Next(dartNum, _dartsLanded);
-
-            Console.WriteLine(randomNum);
+            _genNum = new Random(); // unsure why but the instructions say to
+            _dartNum = dartNum; // initializing _dartNum == dartNum from main
+            dartsLanded = 0; // says to initialize so I did, unsure why
         }
 
         public int getLanded
         {
-            get => _dartsLanded;
-            set => _dartsLanded = value;
+            get => dartsLanded;
+            set => dartsLanded = value;
+        }
+
+        public void throwDarts()
+        {
+            double x = 0.0; // x coordinate on the board
+            double y = 0.0; // y coordinate on the board
+
+            // loop to throw darts at the board at random x and y locations
+            for (int i = 0; i < _dartNum; i++)
+            {
+                // randomizing x and y
+                x = _genNum.NextDouble();
+                y = _genNum.NextDouble();
+
+                x = x * x;
+                y = y * y;
+
+                // checking if the hypotenuse is 1 or lower
+                if (Math.Sqrt(x + y) < 1)
+                {
+                    dartsLanded++;
+                }
+            }
         }
     }
 }
--- CW 9 - Threading/Program.cs
+++ CW 9 - Threading/Program.cs
@@ -1,7 +1,13 @@
-﻿using System;
+﻿/* Author: Jordan Taylor
+ * Date: 03 / 22 / 2021
+ * Description: Throws darts at a board and estimates pi from it. The more you throw the closer you get to pi.
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Threading
@@ -10,9 +16,41 @@ namespace Threading
     {
         static void Main(string[] args)
         {
-            FindPiThread e = new FindPiThread(20);
+            int throwNum = 0; // throws to make
+            int threadNum = 0; // threads to run
+            int dartsInside = 0;
 
-            Console.WriteLine(e.getLanded); // Test, works
+            List<Thread> threads = new List<Thread>();
+            List<FindPiThread> piThread = new List<FindPiThread>();
+
+            Console.WriteLine("Enter how many throws to make ");
+            throwNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter how many threads ");
+            threadNum = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < threadNum; i++)
+            {
+                FindPiThread pi = new FindPiThread(throwNum);
+                piThread.Add(pi);
+
+                Thread thread = new Thread(new ThreadStart(pi.throwDarts));
+                threads.Add(thread);
+                thread.Start();
+                Thread.Sleep(16);
+            }
+            foreach (Thread thread in threads)
+            {
+                thread.Join();
+            }
+            foreach (FindPiThread thread in piThread)
+            {
+                dartsInside += thread.dartsLanded;
+            }
+
+            double total = throwNum * threadNum; // this is to help make the console output a double
+
+            Console.WriteLine(4 * (dartsInside / total));
 
             Console.ReadKey();
         }
